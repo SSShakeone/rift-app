6fe18d10e76e626ce44258078564ec3d9dc88417
Rift <user@rift-app.local>
2026-06-06 18:09:28 +0800

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/rift-app',
+};
 
 export default nextConfig;
